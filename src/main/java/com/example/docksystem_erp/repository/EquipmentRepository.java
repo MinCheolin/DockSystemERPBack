2d0b6ab378cf5ee878eba594180f98c2f25123cf
김민철 <rlaalscjf739789@gmail.com>
2025-09-02 15:18:33 +0900

    담당자를 제외한 나머지 구현 완료

---
+++ src/main/java/com/example/docksystem_erp/repository/EquipmentRepository.java
@@ -0,0 +1,7 @@
+package com.example.docksystem_erp.repository;
+
+import com.example.docksystem_erp.entity.Equipment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EquipmentRepository extends JpaRepository<Equipment,Long> {
+}
