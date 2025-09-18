ba1dbf9aebe0535f4f09fb14caeeb6091db84f8f
unknown <windn0503@gmail.com>
2025-09-19 08:54:24 +0900

    project create

--- src/main/java/com/example/docksystem_erp/dto/ProductPlan/ProductPlanResponseDto.java
+++ src/main/java/com/example/docksystem_erp/dto/ProductPlan/ProductPlanResponseDto.java
@@ -1,5 +1,7 @@
 package com.example.docksystem_erp.dto.ProductPlan;
 
+import com.example.docksystem_erp.dto.BOM.Bom.BOMResponseDto;
+import com.example.docksystem_erp.dto.Project.ProjectResponseDto;
 import com.example.docksystem_erp.entity.BOM.BOM;
 import com.example.docksystem_erp.entity.ProductPlan.ProductPlan;
 import com.example.docksystem_erp.entity.Project.Project;
@@ -13,8 +15,8 @@ public class ProductPlanResponseDto {
     private String ppName;
     private Date ppStartDate;
     private Date ppEndDate;
-//    private ProjectResponseDto project;
-//    private BOMResponseDto bom;
+    private ProjectResponseDto project;
+    private BOMResponseDto bom;
 
     public static ProductPlanResponseDto fromEntity(ProductPlan productPlan){
         ProductPlanResponseDto dto = new ProductPlanResponseDto();
@@ -22,8 +24,8 @@ public class ProductPlanResponseDto {
         dto.setPpName(productPlan.getPpName());
         dto.setPpStartDate(productPlan.getPpStartDate());
         dto.setPpEndDate(productPlan.getPpEndDate());
-//        dto.setProject(ProjectResponseDto.fromEntity(productPlan.getProject()));
-//        dto.setBom(BOMResponseDto.fromEntity(productPlan.getBOM()));
+        dto.setProject(ProjectResponseDto.fromEntity(productPlan.getProject()));
+        dto.setBom(BOMResponseDto.fromEntity(productPlan.getBom()));
         return dto;
     }
 }
