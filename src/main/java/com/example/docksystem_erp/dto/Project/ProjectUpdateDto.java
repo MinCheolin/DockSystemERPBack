17050dde3f77a78f7ae303993bbd1f90676e933d
unknown <windn0503@gmail.com>
2025-09-23 14:48:53 +0900

    project,productplan,department update

--- src/main/java/com/example/docksystem_erp/dto/Project/ProjectUpdateDto.java
+++ src/main/java/com/example/docksystem_erp/dto/Project/ProjectUpdateDto.java
@@ -1,11 +1,13 @@
 package com.example.docksystem_erp.dto.Project;
 
+import com.example.docksystem_erp.dto.ProductPlan.ProductPlanUpdateRequestDto;
 import com.example.docksystem_erp.entity.Customer.Customer;
 import com.example.docksystem_erp.entity.Project.Project;
 import com.example.docksystem_erp.entity.Project.ProjectType;
 import com.example.docksystem_erp.entity.Vessel.Vessel;
 import lombok.Data;
 import java.util.Date;
+import java.util.List;
 
 @Data
 public class ProjectUpdateDto {
@@ -15,9 +17,10 @@ public class ProjectUpdateDto {
     private Date projectEndDate;
     private Long projectPrice;
     private ProjectType type;
-    private String projectDesciption;
+    private String projectDescription;
     private Long customerNo;
     private Long vesselNo;
+    private List<ProductPlanUpdateRequestDto> productPlans;
 
     public Project toEntity(Customer customer, Vessel vessel){
         Project pjt = new Project();
@@ -25,6 +28,7 @@ public class ProjectUpdateDto {
         pjt.setProjectStartDate(this.getProjectStartDate());
         pjt.setProjectEndDate(this.getProjectEndDate());
         pjt.setProjectPrice(this.getProjectPrice());
+        pjt.setProjectDescription(this.getProjectDescription());
         pjt.setType(this.getType());;
         pjt.setCustomer(customer);
         pjt.setVessel(vessel);
--- src/main/java/com/example/docksystem_erp/repository/ProductPlan/ProductPlanRespository.java
+++
@@ -1,7 +0,0 @@
-package com.example.docksystem_erp.repository.ProductPlan;
-
-import com.example.docksystem_erp.entity.ProductPlan.ProductPlan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductPlanRespository extends JpaRepository<ProductPlan, Long> {
-}
