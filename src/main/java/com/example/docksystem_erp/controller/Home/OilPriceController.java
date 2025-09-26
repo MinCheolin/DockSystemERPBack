222b2eb4c886ce9466ac351bfe9d090ed6210445
piaojongwien <jongwon0223@gmail.com>
2025-09-27 08:47:17 +0900

    home 백엔드 구현

--- src/main/java/com/example/docksystem_erp/controller/Home/OilPriceController.java
+++ src/main/java/com/example/docksystem_erp/controller/Home/OilPriceController.java
@@ -1,4 +1,23 @@
 package com.example.docksystem_erp.controller.Home;
 
+import com.example.docksystem_erp.service.Home.OilPriceService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
 public class OilPriceController {
+
+    private final OilPriceService oilPriceService;
+
+    public OilPriceController(OilPriceService oilPriceService) {
+        this.oilPriceService = oilPriceService;
+    }
+
+    @GetMapping("/api/oil-price/latest-all")
+    public Map<String, Object> getLatestOilPrices() {
+        return oilPriceService.getLatestOilPrices();
+    }
 }
